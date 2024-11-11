7d1344ea1e2bcea31dcd5de8c6be48732cd78c0a
Eiju Ito <eijuito@gmail.com>
2024-11-11 16:11:07 -0300

    description update

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -75,7 +75,6 @@ public class BaseTests {
                 ioException.printStackTrace();
             }
             System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
-            }
     }
 
     public WindowManager getWindowManager() {
